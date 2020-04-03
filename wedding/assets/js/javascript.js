39626059ec3fad4bcdb8c81bdcda74c3800b71b2
Kelsie Stevenson <Kelsie.Stevenson@milliman.com>
2020-04-03 15:00:01 -0400

    make covid-19 updates as it impacted our wedding date

--- wedding/assets/js/javascript.js
+++ wedding/assets/js/javascript.js
@@ -73,7 +73,7 @@ $(document).ready(function(){
 /*************/
 
 // Set the date we're counting down to
-var countDownDate = new Date('May 9, 2020 10:30:00').getTime();
+var countDownDate = new Date('July 25, 2020 10:30:00').getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
